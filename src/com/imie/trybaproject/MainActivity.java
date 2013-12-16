7c2594a76606ed107ece37208b990b003b2d2e22
Quentin Bontemps <q.bontemps@gmail.com>
2013-12-16 14:45:05 +0100

    test

--- src/com/imie/trybaproject/MainActivity.java
+++ src/com/imie/trybaproject/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Test","Hello cock sucker!");
+        Log.i("Test","tesst");
     }
 
 
