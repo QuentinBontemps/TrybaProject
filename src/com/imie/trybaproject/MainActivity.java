aed837f1e30fbb9434525cba48ea82ccdd51e4ac
Quentin Bontemps <q.bontemps@gmail.com>
2013-12-16 14:31:39 +0100

    test branch commit

--- src/com/imie/trybaproject/MainActivity.java
+++ src/com/imie/trybaproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.imie.trybaproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Test","Hello cock sucker!");
     }
 
 
