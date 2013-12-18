d1e82fe85df055cdbcfc170ee7593b02d0ee0007
n1n1du44 <antonin.44@hotmail.fr>
2013-12-18 18:51:18 +0100

    Update gitignore

--- src/com/imie/trybaproject/model/StationEnum.java
+++ src/com/imie/trybaproject/model/StationEnum.java
@@ -1,8 +1,12 @@
 package com.imie.trybaproject.model;
 
 public enum StationEnum {
-	DECOUPE ("Découpe",1),
-	FAC ("Fac",2);
+	STOCK ("Stock",1),
+	DECOUPE("Découpe",2),
+	FACONNAGE("Façonnage",3),
+	PEINTURE("Peinture",4),
+	ASSEMBLAGE("Assemblage",5),
+	PRODUCTION("Production",6);
 	
 	  private String name = "";
 	  private int ordre = 0;
