55ec11f66e283e2a36d981060067587a0eb239bd
n1n1du44 <antonin.44@hotmail.fr>
2013-12-28 17:59:58 +0100

    fix merge conflicts

--- src/com/imie/trybaproject/views/fragment/ListUsersFragment.java
+++ src/com/imie/trybaproject/views/fragment/ListUsersFragment.java
@@ -5,12 +5,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-<<<<<<< HEAD
-=======
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
->>>>>>> master
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
